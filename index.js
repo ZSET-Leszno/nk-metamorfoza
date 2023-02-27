ccdb8bcb13e5e828ae363de1b4e4336e1289437c
Dariusz Stakiewicz <staskiewiczdariusz0@gmail.com>
2023-02-27 13:08:35 +0100

    Poprawka

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 function przyciemnienie() {
     var maxHeight = screen.height;
     var scrollHeight = window.scrollY;
-    if (scrollHeight > 20) {
-        var dark = 0.3 - (scrollHeight * 0.00025);
-        console.log(maxHeight);
-        document.getElementById("dark").style = "backdrop-filter: brightness(" + dark + "); -webkit-backdrop-filter: brightness(" + dark + ");";
-    } else if (scrollHeight >= maxHeight || 0.3 - (scrollHeight * 0.00025) < 0) {
-        document.getElementById("dark").style = "backdrop-filter: brightness(0); -webkit-backdrop-filter: brightness(0);";
-    } else {
-        document.getElementById("dark").style = "";
-    }
+    var dark = maxHeight / (maxHeight - scrollHeight) * 0.3 ;
+    document.getElementById("dark").style = "background-color: rgba(0, 0, 0, "+ dark +");"
 }
 
 addEventListener("scroll", (event) => {
     przyciemnienie();
 });
+
+addEventListener("DOMContentLoaded", (event) => {
+    przyciemnienie();
+})
