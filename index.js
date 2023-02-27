f4a9163878b6f5165d50b5e98d813f2b4cb2f4db
Dariusz Staśkiewicz <staskiewiczdariusz0@gmail.com>
2023-02-27 14:59:04 +0100

    Jakaś animacja

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-function przyciemnienie() {
-    var maxHeight = screen.height;
-    var scrollHeight = window.scrollY;
-    var dark = maxHeight / (maxHeight - scrollHeight) * 0.3 ;
-    document.getElementById("dark").style = "background-color: rgba(0, 0, 0, "+ dark +");"
-}
-
 addEventListener("scroll", (event) => {
-    przyciemnienie();
+    dark();
+    if (isInViewport(document.getElementsByClassName('slide')[0])) {
+        console.log("s");
+        slides();
+    }
 });
 
 addEventListener("DOMContentLoaded", (event) => {
-    przyciemnienie();
+    dark();
 })
+
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function dark() {
+    var maxHeight = screen.height;
+    var scrollHeight = window.scrollY;
+    var dark = maxHeight / (maxHeight - scrollHeight * 0.5) * 0.7 ;
+    document.getElementById("dark").style = "background-color: rgba(0, 0, 0, "+ dark +");"
+}
+
+function slides() {
+    console.log("s");
+    document.getElementById('slide-left').style = "transform: translateX(0); -webkit-transform: translateX(0);";
+    document.getElementById('slide-right').style = "transform: translateX(0); -webkit-transform: translateX(0);";
+    document.getElementById('slide-left1').style = "transform: translateX(0); -webkit-transform: translateX(0);";
+    document.getElementById('slide-right2').style = "transform: translateX(0); -webkit-transform: translateX(0);";
+}
