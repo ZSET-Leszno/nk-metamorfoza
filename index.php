cddc07d29526128482514a42f707bed92b83980c
mewash999 <blenciak@wp.pl>
2023-04-18 08:46:17 +0200

    finish2

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="icon" type="image/svg" href="img/logowhite.svg">
+    <link rel="icon" type="image/svg" href="img/logoblack.svg">
     <title>NK Metamorfoza</title>
 </head>
 <body>
